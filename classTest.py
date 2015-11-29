1d4b16e975660d55ee226a48ab4ec5ed3dc4e074
Максим Евсюков <evsyukovmaxim@gmail.com>
2015-11-30 00:16:49 +0600

    throwing & cathing

--- classTest.py
+++ classTest.py
@@ -16,14 +16,9 @@ class Factory():
 			myevenClass = evenClass()
 			return myevenClass
 		else:
-			try:
-				raise ValueError('Exception masage')
-			except ValueError as err:
-				print(err.args)
-				print "Usual print via Else"
-			
-	factory = staticmethod(factory)
+			raise AttributeError('This nonExisting object and raising especial AttributeError')
 
+	factory = staticmethod(factory)
 
 
 class oddClass():
@@ -53,14 +48,13 @@ evenClass.process()
 anyClass = Factory.factory("any")
 anyClass.process()
 
-## Сделай так, чтобы код внизу выполнился
 
-# Now I'm trying to create a non-existing class name
-nonExists = Factory.factory("SHOOT")
-# And now I'm trying to call process() on non-existing bject
-nonExists.process()
+try:
+	nonExists = Factory.factory("SHOOT")
+	nonExists.process()
+except AttributeError as err:
+	print err.args
 
-## Далее обычный код, он должен выполниться 
 
 now = datetime.datetime.now()
 print "Current date and time using strftime:"
