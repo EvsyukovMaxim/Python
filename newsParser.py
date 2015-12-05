718c7cbc55a555e254942078c6b4a66189d32a8a
Максим Евсюков <evsyukovmaxim@gmail.com>
2015-12-06 00:05:01 +0600

    Finall before makinf parser

--- newsParser.py
+++ newsParser.py
@@ -16,6 +16,7 @@ class NewsStrategyException(Exception):
 class NewsStrategy():
 	@staticmethod
 	def getStrategy(newsSource):
+
 		if newsSource == 'Village':
 			myVillageStrategy = VillageStrategy()
 			return myVillageStrategy
@@ -32,10 +33,16 @@ class VillageStrategy():
 		myNewsObject = NewsObject()
 		myNewsObject.title = "Moscow vs Batman"
 		myNewsObject.preview = "Spider-Man was seen in Novosibirsk"
+		myNewsObject1 = NewsObject()
+		myNewsObject1.title = "mosmosmos"
+		myNewsObject1.preview = "spispispis"
+		myNewsObject2 = NewsObject()
+		myNewsObject2.title = "vilvilvil"
+		myNewsObject2.preview = "vilagevilagevilage"
 		villageList = []
 		villageList.append(myNewsObject)
-		print myNewsObject.title
-		print myNewsObject.preview
+		villageList.append(myNewsObject1)
+		villageList.append(myNewsObject2)
 		return villageList
 
 		
@@ -45,10 +52,16 @@ class AfishaStrategy():
 		myNewsObject = NewsObject()
 		myNewsObject.title = "There was Hitler cought under the bridge"
 		myNewsObject.preview = "Hitler got a tail!!!"
+		myNewsObject1 = NewsObject()
+		myNewsObject1.title = "blabla"
+		myNewsObject1.preview = "tratata"
+		myNewsObject2 = NewsObject()
+		myNewsObject2.title = "tururu"
+		myNewsObject2.preview = "klaklakla"
 		afishaList = []
 		afishaList.append(myNewsObject)
-		print myNewsObject.title
-		print myNewsObject.preview
+		afishaList.append(myNewsObject1)
+		afishaList.append(myNewsObject2)
 		return afishaList
 
 class NewsObject():
@@ -57,8 +70,11 @@ class NewsObject():
 
 
 try:
-	VillageStrategy = NewsStrategy.getStrategy(newsSource)
-	VillageStrategy.parse()
+	Strategy = NewsStrategy.getStrategy(newsSource)
+	var = Strategy.parse()
+	for news in var:
+		print news.title
+		print news.preview
 except NewsStrategyException as err:
 	print err.args
 	sys.exit()
