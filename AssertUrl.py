163a9029ad2e3392d6ef2d13e73732a3c4860516
Максим Евсюков <evsyukovmaxim@gmail.com>
2015-12-06 00:14:10 +0600

    finall

--- AssertUrl.py
+++ AssertUrl.py
@@ -21,7 +21,7 @@ class AssertUrlTestCase(unittest.TestCase):
         # click company name
         driver.find_element_by_css_selector('#module-1-12-1-1-1-3 > div > header > a').click()
         # assert current_url
-        self.assertEquals(driver.current_url, "http://2gis.ru/novosibirsk/search/%D0%A8%D0%A2%D0%A0%D0%98%D0%A5-%D0%9C%20%D0%9D%D0%BE%D0%B2%D0%BE%D1%81%D0%B8%D0%B1%D0%B8%D1%80%D1%81%D0%BA/firm/141265769449200/tab/firms/zoom/11");
+        self.assertEquals(driver.current_url, "http://2gis.ru/novosibirsk/search/%D0%A8%D0%A2%D0%A0%D0%98%D0%A5-%D0%9C%20%D0%9D%D0%BE%D0%B2%D0%BE%D1%81%D0%B8%D0%B1%D0%B8%D1%80%D1%81%D0%BA/firm/141265769449200/tab/firms/zoom/11")
     
     def tearDown(self):
         self.driver.quit()
